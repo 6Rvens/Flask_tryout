665535ae05a0cf84523d276c449b353b719262a6
6Rvens <sdaamen9@gmail.com>
2024-03-05 11:58:50 +0100

    basic Flask code

--- falsk_app.py
+++ falsk_app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '<p>Hello world!</p>'
+    return '<p>wereld van de computer!</p>'
 
 if __name__ == '__main__':
     app.run()
